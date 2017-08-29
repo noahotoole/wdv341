4ef7e45a674987e1c60252d5fbb955854a49a492
Noah O'Toole <31321438+noahotoole@users.noreply.github.com>
2017-08-29 12:50:40 -0500

    Add files via upload

---
+++ testGit.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Noah O'Toole's WDV 341 Homework - Test Git</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  
+  <body>
+	<div class="off-canvas-wrapper">
+		
+	
+	<!-- Off Canvas Menu -->
+			<div class="off-canvas position-left" id="mobile-menu" data-off-canvas>
+				<ul>
+					<li><a href="../index.html">Home</a></li>
+				</ul>
+			</div>
+		
+	<!-- Mobile Navigation -->	
+			<div class="off-canvas-content" data-off-canvas-content>
+				<div class="title-bar hide-for-medium">
+					<div class="title-bar-left">
+						<button class="menu-icon" type="button" data-open="mobile-menu"></button>
+						<span class="title-bar-title">MENU</span>
+					</div>
+				</div>
+	
+    <!-- Desktop Navigation -->	
+				<nav class="top-bar nav-desktop">
+					<div class="wrap">
+
+						<div class="top-bar-left">
+							<h3 class="site-logo">Portfolio by Noah O'Toole</h3>
+						</div>
+						<div class="top-bar-right">
+							<ul class="menu menu-desktop">
+								<li><a href="../index.html">Home</a></li>
+							</ul>
+						</div>
+					</div>  
+				</nav>
+
+	<!-- Hero Section -->
+				<section class="hero">
+					<div class="wrap">
+						<h1>WDV 341 HOMEWORK</h1>
+						<p> </p>
+					</div>
+				</section>
+	
+	<!-- Main Section -->	
+				<section class="main">
+					<div class="wrap row">
+						<div class="small-12 medium-7 column">
+							<h1>Test Git</h1>
+							<hr>
+							<p>This is a test by Noah O'Toole </p>	
+						</div>
+						
+				</section>	
+	
+	<!-- Footer -->
+	
+				<footer>
+					<div class="wrap row small-up-1 medium-up-3">
+						<div class="column">
+							<h4>Contact Info</h4>
+							<hr>
+							<a href="#"><span>Phone</span> 515 490 4842</a>
+							<a href="#"><span>Email</span> noahotoole@gmail.com</a>
+							<a href="#"><span>Address</span> West Des Moines, Iowa</a>
+						</div>
+	
+						<div class="column">
+							<h4>Site Map</h4>
+							<hr>
+							<a href="#">About Me</a>
+							<a href="#">Services</a>
+							<a href="#">Contact Me</a>
+						</div>
+	
+						<div class="column">
+							<h4>Social Media</h4>
+							<hr>
+							<a href="#">Facebook</a>
+							<a href="#">Twitter</a>
+							<a href="#">Instagram</a>
+						</div>
+					</div>
+				</footer>
+				
+			</div>
+		</div>
+	</div>
+
+	
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
